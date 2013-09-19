d8575b8795c8b9a2e07679a38da91fc6b8844da7
Maciek Gajewski <maciej.gajewski0@gmail.com>
2013-09-19 16:38:18 +0200

    one more test case

--- test/main.cpp
+++ test/main.cpp
@@ -7,9 +7,20 @@
 
 using namespace coroutines;
 
+template<typename T1, typename T2>
+void _TEST_EQUAL(const T1& a, const T2& b, const char* msg)
+{
+    if (a != b)
+    {
+        std::cout << msg << " failed" << std::endl;
+        assert(false);
+    }
+}
+#define TEST_EQUAL(a, b) _TEST_EQUAL(a, b, #a "==" #b)
+
 // simple reader-wrtier test, wrtier closes before reader finishes
 // expected: reader will read all data, and then throw channel_closed
-void test_rw()
+void test_reading_after_close()
 {
     std::unique_ptr<threaded_scheduler> sched(new threaded_scheduler);
     set_scheduler(sched.get());
@@ -23,20 +34,16 @@ void test_rw()
     // writer coroutine
     go([&last_wrtitten](channel_writer<int>& writer)
     {
-        std::cout << "writing started" << std::endl;
         for(int i = 0; i < 5; i++)
         {
             writer.put(i);
             last_wrtitten = i;
         }
-        std::cout << "writing completed" << std::endl;
-
     }, std::move(pair.writer));
 
     // reader coroutine
     go([&last_read](channel_reader<int>& reader)
     {
-        std::cout << "reader started" << std::endl;
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
         for(;;)
         {
@@ -49,7 +56,6 @@ void test_rw()
                 break;
             }
         }
-        std::cout << "reader finished" << std::endl;
     }, std::move(pair.reader));
 
 
@@ -57,12 +63,46 @@ void test_rw()
     set_scheduler(nullptr);
     sched.reset();
 
-    assert(last_wrtitten == 4);
-    assert(last_read == 4);
+    TEST_EQUAL(last_wrtitten, 4);
+    TEST_EQUAL(last_read, 4);
+}
+
+// Reader blocking:  reader should block until wrtier writes
+void test_reader_blocking()
+{
+    std::unique_ptr<threaded_scheduler> sched(new threaded_scheduler);
+    set_scheduler(sched.get());
+
+    // create channel
+    channel_pair<int> pair = make_channel<int>(10);
+
+    bool reader_finished = false;
+    go([&reader_finished](channel_reader<int>& r)
+    {
+        r.get();
+        reader_finished = true;
+    }, std::move(pair.reader));
+
+    TEST_EQUAL(reader_finished, false);
+
+    bool writer_finished = false;
+    go([&writer_finished](channel_writer<int>& w)
+    {
+        w.put(7);
+        writer_finished = true;
+
+    }, std::move(pair.writer));
+
+    set_scheduler(nullptr);
+    sched.reset();
+
+    TEST_EQUAL(reader_finished, true);
+    TEST_EQUAL(writer_finished, true);
 }
 
 int main(int , char** )
 {
-    test_rw();
+    test_reading_after_close();
+    test_reader_blocking();
 }
 
